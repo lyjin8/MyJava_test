e76f9e34a2f2352a4893e9eab9e4414e02d3fb6e
lyjin8 <scdjsly@163.com>
2025-05-22 10:10:20 +0800

    2025/5/22

--- src/ly/set/hash/object/Person.java
+++ src/ly/set/hash/object/Person.java
@@ -1,5 +1,7 @@
 package ly.set.hash.object;
 
+import java.util.Objects;
+
 public class Person {
     private String name;
     private int age;
@@ -28,4 +30,23 @@ public class Person {
         this.age = age;
     }
 
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
 }
